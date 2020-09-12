283b6d827e82e47a3cf0681f2839b37d81a291d1
Angel Angelov <aangeloo@gmail.com>
2020-09-12 21:28:45 +0200

    add step 1 - merge reads

--- bin/01_merge_reads.R
+++ bin/01_merge_reads.R
@@ -3,27 +3,26 @@
 # step 1
 # merge reads
 
-# args 1 is a folder with fastq file pairs from a 16S amplicon dataset
-# args 2 is separator used delimiting sample id from the rest of FASTQ filename
+# args are the fastq files
 
 require(dada2)
 require(rexmap)
 
 args <- commandArgs(trailingOnly = T)
 
-# read fastq files and get sample ids
-fq_fwd <- read_files(args[1], 'R1')
-fq_rev <- read_files(args[1], 'R2')
+# 
+fq_fwd <- args[grepl("R1", args)]
+fq_rev <- args[grepl("R2", args)]
 
-sample_ids <- rexmap::sampleids_from_filenames(fq_fwd, separator = args[2]) ###################TODO check if unique
+sample_ids <- rexmap::sampleids_from_filenames(fq_fwd, separator = "_") ###################TODO check if unique
 fq_merged <- file.path(paste0(sample_ids, '_merged.fastq'))
 
 # actual merge
-mergestats <- rexmap::merge_pairs(fq_fwd, fq_rev, fq_mer, verbose=TRUE, timing = TRUE)
+mergestats <- rexmap::merge_pairs(fq_fwd, fq_rev, fq_merged, verbose=TRUE, timing = TRUE)
 
 mergestatsdf <- data.frame(
 	sample_ids, 
-	total = as.numeric(mergestats[1, ]), 
+	total_reads = as.numeric(mergestats[1, ]), 
 	low_pct_sim = as.numeric(mergestats[2, ]), 
 	low_aln_len = as.numeric(mergestats[3, ]))
 
--- main.nf
+++ main.nf
@@ -12,10 +12,34 @@ if( !nextflow.version.matches('>=19.08') ) {
  * pipeline input parameters 
  */
 
-params.readsdir = "fastq"
-params.outdir = "${params.readsdir}/results-rexmap"
+params.fastqdir = "fastq"
+params.outdir = "$workflow.launchDir/results-rexmap"
 params.fqpattern = "*_R{1,2}_001.fastq.gz"
 
 params.help = ""
 
-mqc_config = file(params.multiqc_config) // this is needed, otherwise the multiqc config file is not available in the docker image
+//just in case trailing slash in readsdir not provided...
+fastqdir_repaired = "${params.fastqdir}".replaceFirst(/$/, "/") 
+fastqfiles = fastqdir_repaired + params.fqpattern
+
+// channel for reads folder --> seqtools
+Channel
+    .fromFilePairs( fastqfiles, checkIfExists: true )
+    .set { fastq_ch }
+
+process merge_reads {
+    tag "$sample_id"
+    publishDir "${params.outdir}/merged_reads", mode: 'copy'
+    
+    input:
+        tuple sample_id, file(x) from fastq_ch
+    
+    output:
+        file '*_merged.fastq' into merged_reads_ch
+    
+    script:
+    """
+    01_merge_reads.R ${x}
+    """
+
+}
