dc058de2cf9c0b068ae56f87e778d2d87f9bd4e5
Angel Angelov <aangeloo@gmail.com>
2020-09-14 15:46:41 +0200

    reorganize steps, merge and trim in one process

---
+++ bin/01_merge_and_trim.R
@@ -0,0 +1,49 @@
+#!/usr/bin/env Rscript
+
+# step 1
+# merge reads and trim primers from merged reads
+
+# args are the fastq files
+# artefacts are 
+
+#require(dada2)
+require(rexmap)
+require(argparse)
+
+parser <- ArgumentParser(description='merge and primer-trim fastq files')
+parser$add_argument('fastqfiles', metavar = "fastqfiles", nargs = '+', help = 'fastq files')
+parser$add_argument('--region', default = 'V3-V4', 
+	help = 'Hypervariable region used. Used to automatically retrieve PCR primer sequences for trimming. Can be one of V1-V3, V3-V4 or V4')
+# https://docs.python.org/3/library/argparse.html#argparse.ArgumentParser.add_argument
+
+args <- parser$parse_args()
+
+# 
+fq_fwd <- args$fastqfiles[grepl("R1", args$fastqfiles)]
+fq_rev <- args$fastqfiles[grepl("R2", args$fastqfiles)]
+# 
+sample_ids <- rexmap::sampleids_from_filenames(fq_fwd, separator = "_") ###################TODO check if unique
+fq_merged <- file.path(paste0(sample_ids, '_merged.fastq'))
+fq_pcrtrimmed <- file.path(paste0(sample_ids, '_trimmed.fastq'))
+# 
+# actual merge
+mergestats <- rexmap::merge_pairs(fq_fwd, fq_rev, fq_merged, verbose=TRUE, timing = TRUE)
+trimstats  <- rexmap::remove_pcr_primers(fq_merged, fq_pcrtrimmed, region = args$region, verbose = TRUE, timing = TRUE)
+# 
+mergestatsdf <- data.frame(
+	sample_ids = sample_ids,
+	total_reads = as.numeric(mergestats[1, ]),
+	low_pct_sim = as.numeric(mergestats[2, ]),
+	low_aln_len = as.numeric(mergestats[3, ])
+)
+# 
+trimstatsdf <- data.frame(
+	sample_ids = sample_ids,
+	total = as.numeric(mergestats[1, ]),
+	fwd_trim = as.numeric(trimstats[1,]),
+	rev_trim = as.numeric(trimstats[2,])
+	)
+# 
+# 
+write.table(mergestatsdf, file = "merge-stats.csv", row.names = FALSE, col.names = FALSE)
+write.table(trimstatsdf, file = "primertrim-stats.csv", row.names = FALSE, col.names = FALSE)
--- main.nf
+++ main.nf
@@ -1,5 +1,4 @@
 // nextflow-rexmap pipelen
-
 /*
 NXF ver 19.08+ needed because of the use of tuple instead of set
 */
@@ -22,24 +21,40 @@ params.help = ""
 fastqdir_repaired = "${params.fastqdir}".replaceFirst(/$/, "/") 
 fastqfiles = fastqdir_repaired + params.fqpattern
 
-// channel for reads folder --> seqtools
+// channel for fastq files --> merge and trim
 Channel
     .fromFilePairs( fastqfiles, checkIfExists: true )
     .set { fastq_ch }
 
-process merge_reads {
+// step 1 --> merge reads and trim primers
+process merge_and_trim {
     tag "$sample_id"
-    publishDir "${params.outdir}/merged_reads", mode: 'copy'
+    publishDir "${params.outdir}/merged_trimmed_reads", mode: 'copy', pattern: '*_{trimmed,merged}.fastq'
     
     input:
         tuple sample_id, file(x) from fastq_ch
-    
     output:
-        file '*_merged.fastq' into merged_reads_ch
-    
+        file '*_{trimmed,merged}.fastq' into mergetrim_ch
+        file 'merge-stats.csv' into mergestats_ch
+        file 'primertrim-stats.csv' into trimstats_ch
     script:
     """
-    01_merge_reads.R ${x}
+    01_merge_and_trim.R ${x}
     """
-
 }
+
+process merge_and_trim_stats {
+    publishDir params.outdir, mode: 'copy'
+
+    input:
+        file 'merge-stats*' from mergestats_ch.collect()
+        file 'primertrim-stats*' from trimstats_ch.collect()
+    output:
+        file '*stats.csv'
+    script:
+    """
+    cat merge-stats* > merge-reads-stats.csv
+    cat primertrim-stats* > primertrim-stats.csv
+    """
+}
+
